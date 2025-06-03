3817d7358c5f17ec63ae4cbbd0283d5be988c2d8
corexaen <88080149+corexaen@users.noreply.github.com>
2025-06-03 18:52:32 +0900

    Delete server/src/config directory

--- server/src/config/kakao.js
+++
@@ -1,6 +0,0 @@
-//카카오 api에서 사용할 키
-
-const JAVASCRIPT_KEY = '867e433d3e155c0b69571f05c1fdad6a';
-const REDIRECT_URI = 'https://gachitda.corexaen.com/api/auth/kakao/callback';
-const REST_API_KEY = '6c3374b50f58aab435f2a14ee1b68f64';
-module.exports = { JAVASCRIPT_KEY, REDIRECT_URI, REST_API_KEY };
